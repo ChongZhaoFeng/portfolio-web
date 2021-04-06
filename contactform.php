9944dfa8ec648a914b18c847c757bcdbca43d1eb
ZhaoFeng Chong <zfchong009@gmail.com>
2021-04-06 20:41:04 +0800

    php

--- contactform.php
+++ contactform.php
@@ -12,4 +12,19 @@ if ($conn->connect_error){
 }
 echo "Connected Successfully"
 
+
+$firstname = $_REQUEST['firstname'];
+$lastname = $_REQUEST['lastname'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+
+$query=mysqli_query($conn,"INSERT INTO contactform(firstname, lastname, email, description) 
+                            VALUES ('$firstname'),('$lastname'),('$email'),('$subject') ") 
+                            or die(mysqli_error($conn));
+
+mysqli_close($conn);
+
+header("location:index.html?note=success");
+
+
 ?>
