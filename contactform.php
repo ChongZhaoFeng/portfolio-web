d97cca621711a50ca5fff4570b9dc85b936882dc
ZhaoFeng Chong <zfchong009@gmail.com>
2021-04-06 21:33:33 +0800

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,6 +1,16 @@
 <?php
 
-include('connectdb.php');
+$dbServername = 'localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'portfolio';
+
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+
+if ($conn->connect_error){
+    die("Connection failed: ". $conn->connect_error);
+}
+echo "Connected Successfully";
 
 
 $firstname = $_POST['firstname'];
