ec31377196aada3852805d20428e1d42f54fc184
ZhaoFeng Chong <zfchong009@gmail.com>
2021-04-06 21:12:00 +0800

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -19,7 +19,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 
 $query=mysqli_query($conn,"INSERT INTO contactform (firstname, lastname, email, description)
-VALUES ('$firstname', '$lastname', '$email','$subject)"; 
+VALUES ('$firstname', '$lastname', '$email','$subject)") 
                             or 
                             die(mysqli_error($conn));
 
