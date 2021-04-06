13955262e3bf093f8b26d93e28d75ce08ddd0ced
ZhaoFeng Chong <zfchong009@gmail.com>
2021-04-06 21:44:21 +0800

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -8,8 +8,8 @@ $lastname = $_POST['lastname'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 
-$query=mysqli_query($conn,"INSERT INTO contactform (firstname, lastname, email, description)
-VALUES ('$firstname', '$lastname', '$email','$subject)") or die (mysqli_error($conn));
+$query="INSERT INTO contactform (firstname, lastname, email, description)
+        VALUES ('$firstname', '$lastname', '$email','$subject')" or die (mysqli_error($conn));
                             
 mysqli_close($conn);
 
