edf55f6776553b845daf72f461512f3cbb9b8b74
ZhaoFeng Chong <zfchong009@gmail.com>
2021-04-06 20:48:45 +0800

    php

--- contactform.php
+++ contactform.php
@@ -10,17 +10,27 @@ $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
 if ($conn->connect_error){
     die("Connection failed: ". $conn->connect_error);
 }
-echo "Connected Successfully"
+echo "Connected Successfully";
 
 
-$firstname = $_REQUEST['firstname'];
-$lastname = $_REQUEST['lastname'];
-$email = $_REQUEST['email'];
-$subject = $_REQUEST['subject'];
+$firstname = $POST['firstname'];
+$lastname = $POST['lastname'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
 
-$query=mysqli_query($conn,"INSERT INTO contactform(firstname, lastname, email, description) 
-                            VALUES ('$firstname'),('$lastname'),('$email'),('$subject') ") 
-                            or die(mysqli_error($conn));
+$query=mysqli_query($conn,"INSERT INTO 
+                            'contactform'(
+                                firstname, 
+                                lastname, 
+                                email, 
+                                description) 
+                            VALUES 
+                            ('$firstname'),
+                            ('$lastname'),
+                            ('$email'),
+                            ('$subject'); ") 
+                            or 
+                            die(mysqli_error($conn));
 
 mysqli_close($conn);
 
